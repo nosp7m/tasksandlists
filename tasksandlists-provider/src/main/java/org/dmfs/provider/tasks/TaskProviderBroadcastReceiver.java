284dd0f488623a858a87450e5c82a10f9e2dc511
nosp7m <13512273+nosp7m@users.noreply.github.com>
2026-03-04 21:01:45 +0100

    Add missing "android.permission.USE_EXACT_ALARM".
    Seems to be required for apps targeting API 31+.

--- tasksandlists-provider/src/main/java/org/dmfs/provider/tasks/TaskProviderBroadcastReceiver.java
+++ tasksandlists-provider/src/main/java/org/dmfs/provider/tasks/TaskProviderBroadcastReceiver.java
@@ -57,7 +57,7 @@ public class TaskProviderBroadcastReceiver extends BroadcastReceiver
         Intent alarmIntent = new Intent(context, TaskProviderBroadcastReceiver.class);
         alarmIntent.setAction(ACTION_NOTIFICATION_ALARM);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REQUEST_CODE_ALARM, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REQUEST_CODE_ALARM, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         // cancel any previous alarm
         am.cancel(pendingIntent);
